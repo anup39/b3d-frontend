36a11d552cc0c6e9493a7edc1308147cabf151bd
Anup Dahal <adahal39@gmail.com>
2023-07-12 09:30:08 +0545

    Geocoder added in map

--- src/map/Map.jsx
+++ src/map/Map.jsx
@@ -1,8 +1,12 @@
 import { useRef, useEffect, useState } from "react";
 import maplibregl from "maplibre-gl";
 import "./Map.scss";
+import MaplibreGeocoder from "@maplibre/maplibre-gl-geocoder";
+import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
+import GeocoderApi from "../maputils/GeocoderApi";
+import PropTypes from "prop-types";
 
-export default function Map() {
+export default function Map({ refObj }) {
   const mapContainer = useRef(null);
   const [map, setMap] = useState();
 
@@ -27,5 +31,25 @@ export default function Map() {
     };
   }, []);
 
+  useEffect(() => {
+    if (map) {
+      const geocoder = new MaplibreGeocoder(GeocoderApi, {
+        maplibregl: maplibregl,
+        showResultsWhileTyping: true,
+        flyTo: true,
+      });
+
+      geocoder.addTo(refObj.current);
+      geocoder.on("result", function (ev) {
+        const coords = ev.result.geometry.coordinates;
+        map.flyTo({ center: coords });
+      });
+    }
+  }, [map, refObj]);
+
   return <div ref={mapContainer} id="map" className="map" />;
 }
+
+Map.propTypes = {
+  refObj: PropTypes.object,
+};
--- src/pages/MapSection.jsx
+++ src/pages/MapSection.jsx
@@ -1,9 +1,16 @@
+import { useRef } from "react";
 import Map from "../map/Map";
+import Search from "../components/Search/Search";
 
 export default function MapSection() {
+  const searchRef = useRef(null);
+
   return (
     <div>
-      <Map />
+      <Map refObj={searchRef} />
+      <div style={{ position: "absolute", top: "2vh", left: "2vw" }}>
+        <Search refObj={searchRef} />
+      </div>
     </div>
   );
 }
