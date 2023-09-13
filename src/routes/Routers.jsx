4f1a03806939123f8bf2bafcd6c646c2e844c7b1
Anup Dahal <adahal39@gmail.com>
2023-09-13 17:36:44 +0545

    Create a form component

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import MapSection from "../pages/MapSection";
+import DashBoard from "../pages/DashBoard";
 
 export default function Routers() {
   return (
@@ -8,6 +9,7 @@ export default function Routers() {
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route exact path="/map" element={<MapSection />} />
+        <Route exact path="/dashboard" element={<DashBoard />} />
       </Routes>
     </Router>
   );
