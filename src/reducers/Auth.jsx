775accf6af05ca7f165355416c9f08c9b43d4af9
Anup Dahal <adahal39@gmail.com>
2023-10-06 06:54:39 +0545

    Added a reducer for Auth

--- src/reducers/Auth.jsx
+++ src/reducers/Auth.jsx
@@ -1,21 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  username: "anup",
+  token: "",
+  user_id: "",
+  user_name: "",
 };
 
 export const Auth = createSlice({
   name: "Auth",
   initialState,
   reducers: {
-    toggleUsername: (state, action) => {
-      state.username = action.payload;
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    setUserId: (state, action) => {
+      state.user_id = action.payload;
+    },
+    setUserName: (state, action) => {
+      state.user_name = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
 // eslint-disable-next-line react-refresh/only-export-components
-export const { toggleUsername } = Auth.actions;
+export const { setToken, setUserId, setUserName } = Auth.actions;
 
 export default Auth.reducer;
