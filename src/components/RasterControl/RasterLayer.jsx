f88cc77651fde3dd3585a6637e0f4fb9f73b1c6c
Anup Dahal <adahal39@gmail.com>
2023-10-09 18:44:24 +0545

    tile integration

--- src/components/RasterControl/RasterLayer.jsx
+++ src/components/RasterControl/RasterLayer.jsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -25,43 +24,60 @@ export default function RasterLayer({ map, project_id }) {
     setChecked(newChecked);
 
     if (newChecked.length === 2) {
-      // const map = window.map_global;
-      map.addSource("wms-test-source", {
+      console.log("checked");
+
+      axios
+        .get(
+          `${
+            import.meta.env.VITE_API_DASHBOARD_URL
+          }/projects/${project_id}/rasters/${ortho.id}/orthophoto/metadata/`
+        )
+        .then((res) => {
+          if (res.data.bounds.value) {
+            map.fitBounds([
+              [-107.88857386093723, 38.98669825126662], // southwestern corner of the bounds
+              [-107.8871274949127, 38.98772137388136], // northeastern corner of the bounds
+            ]);
+          }
+        });
+
+      map.addSource(`${ortho.uuid}-source`, {
         type: "raster",
         // use the tiles option to specify a WMS tile source URL
         // https://docs.mapbox.com/mapbox-gl-js/style-spec/sources/
         tiles: [
-          "http://localhost:8080/geoserver/super_admingeoservertest/wms?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=super_admingeoservertest:orthophoto",
+          `${
+            import.meta.env.VITE_API_DASHBOARD_URL
+          }/projects/${project_id}/rasters/${
+            ortho.id
+          }/orthophoto/tile/{z}/{x}/{y}/`,
         ],
-        tileSize: 256,
+        tileSize: 512,
       });
 
       map.addLayer({
-        id: "wms-test-layer",
+        id: `${ortho.uuid}-layer`,
         type: "raster",
-        source: "wms-test-source",
-        paint: {},
+        source: `${ortho.uuid}-source`,
+        minzoom: 0,
+        maxzoom: 24,
       });
-
-      map.fitBounds([
-        [-107.88857386093723, 38.98669825126662], // southwestern corner of the bounds
-        [-107.8871274949127, 38.98772137388136], // northeastern corner of the bounds
-      ]);
     } else {
-      const map = window.map_global;
+      console.log("uncheked");
+      console.log(ortho);
       const style = map.getStyle();
       const existingLayer = style.layers.find(
-        (layer) => layer.id === "wms-test-layer"
+        (layer) => layer.id === `${ortho.uuid}-layer`
       );
-      const existingSource = style.sources["wms-test-source"];
+      const existingSource = style.sources[`${ortho.uuid}-source`];
       if (existingLayer) {
         // remove the layer from the map
-        map.off("click", "wms-test-layer");
-        map.removeLayer("wms-test-layer");
+        map.off("click", `${ortho.uuid}-layer`);
+        map.removeLayer(`${ortho.uuid}-layer`);
       }
       if (existingSource) {
         // remove the source from the map
-        map.removeSource("wms-test-source");
+        map.removeSource(`${ortho.uuid}-source`);
       }
     }
   };
