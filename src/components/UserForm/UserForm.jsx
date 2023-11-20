10f78daecc294cada1d6326f011a922498128277
Anup Dahal <adahal39@gmail.com>
2023-11-20 18:51:30 +0545

    Added a drop down for role selection

---
+++ src/components/UserForm/UserForm.jsx
@@ -0,0 +1,260 @@
+import Tooltip from "@mui/material/Tooltip";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { setUsers } from "../../reducers/Users";
+import {
+  setshowToast,
+  settoastMessage,
+  settoastType,
+} from "../../reducers/DisplaySettings";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Autocomplete } from "@mui/material";
+
+export default function UserForm() {
+  const dispatch = useDispatch();
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const user_id = useSelector((state) => state.auth.user_id);
+  const [roles, setRoles] = useState([]);
+
+  console.log(inputValue, "inputValue");
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_DASHBOARD_URL}/global-roles/`)
+      .then((res) => {
+        console.log(res.data);
+        setRoles(res.data);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleCreateClient = (event) => {
+    // TODO : Here handle the error since username and email  are checked in backend
+    event.preventDefault();
+    setLoading(true);
+    const data_ = new FormData(event.currentTarget);
+    console.log(data_, "data");
+    // axios
+    //   .post(`${import.meta.env.VITE_API_DASHBOARD_URL}/clients/`, data_)
+    //   .then(() => {
+    //     setLoading(false);
+    //     dispatch(setshowToast(true));
+    //     dispatch(settoastMessage("Successfully Created User"));
+    //     dispatch(settoastType("success"));
+    //     closeForm();
+    //     axios
+    //       .get(`${import.meta.env.VITE_API_DASHBOARD_URL}/clients/`, {
+    //         // headers: {
+    //         //   Authorization: "Token " + localStorage.getItem("token"), // Include the API token from localStorage in the 'Authorization' header
+    //         // },
+    //       })
+    //       .then((res) => {
+    //         dispatch(setUsers(res.data));
+    //       });
+    //   })
+    //   .catch((error) => {
+    //     const error_message = error.response.data.message;
+    //     setLoading(false);
+    //     dispatch(setshowToast(true));
+    //     dispatch(
+    //       settoastMessage(
+    //         error_message ? error_message : "Failed to Create User"
+    //       )
+    //     );
+    //     dispatch(settoastType("error"));
+    //   });
+  };
+
+  const openForm = () => {
+    setIsFormOpen(true);
+  };
+
+  const closeForm = () => {
+    setIsFormOpen(false);
+  };
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  return (
+    <>
+      <Tooltip title="Create User">
+        <Button
+          onClick={openForm}
+          sx={{ margin: "5px" }}
+          variant="contained"
+          color="error"
+        >
+          Create User
+        </Button>
+      </Tooltip>
+      {isFormOpen && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0, 0, 0, 0.5)",
+            zIndex: 9999,
+          }}
+        >
+          <form
+            onSubmit={handleCreateClient}
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "300px",
+              background: "#fff",
+              padding: "20px",
+              zIndex: 10000,
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="username"
+                  name="username"
+                  autoComplete="username"
+                  autoFocus
+                  size="small"
+                  InputLabelProps={{ shrink: true }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="firstname"
+                  name="firstname"
+                  label="First Name"
+                  variant="outlined"
+                  size="small"
+                  InputLabelProps={{ shrink: true }}
+                  required
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="lastname"
+                  name="lastname"
+                  label="Last Name"
+                  variant="outlined"
+                  size="small"
+                  InputLabelProps={{ shrink: true }}
+                  required
+                  fullWidth
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="email"
+                  name="email"
+                  autoComplete="email"
+                  size="small"
+                  InputLabelProps={{ shrink: true }}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  autoComplete="current-password"
+                  size="small"
+                  InputLabelProps={{ shrink: true }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          onClick={handleTogglePasswordVisibility}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <Autocomplete
+                  disablePortal
+                  id="user-roles"
+                  options={roles}
+                  getOptionLabel={(option) => option.name}
+                  style={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Select User Role"
+                      InputLabelProps={{ shrink: true }}
+                      variant="outlined"
+                      size="small"
+                      required
+                    />
+                  )}
+                  onChange={(event, newValue) => {
+                    if (newValue) {
+                      setInputValue(newValue);
+                    }
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant={loading ? "outlined" : "contained"}
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  {loading ? null : "Create User"}
+                  {loading ? <CircularProgress /> : null}
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  onClick={closeForm}
+                  variant="contained"
+                  color="error"
+                  size="small"
+                  fullWidth
+                >
+                  Close
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      )}
+    </>
+  );
+}
--- src/pages/ManageUsers.jsx
+++ src/pages/ManageUsers.jsx
@@ -2,15 +2,10 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import AppBar from "../components/AppBar/AppBar";
-import Box from "@mui/material/Box";
-import UserTransferList from "../components/UserTransferList/UserTransferList";
-import { Button, Tooltip } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import UserCard from "../components/UserCard/UserCard";
-import ClientForm from "../components/ClientForm/ClientForm";
+import UserForm from "../components/UserForm/UserForm";
 
 export default function ManageUsers() {
-  const navigate = useNavigate();
   const { client_id } = useParams();
   const [users, setUsers] = useState([]);
 
@@ -31,24 +26,7 @@ export default function ManageUsers() {
     <>
       <AppBar></AppBar>
 
-      <ClientForm />
-
-      {/* <Tooltip>
-        <Button
-          onClick={() => navigate("/register")}
-          sx={{
-            //   marginBottom: "15px",
-            marginTop: "5px",
-            marginLeft: "5px",
-            marginRight: "5px",
-          }}
-          variant="contained"
-          color="error"
-        >
-          Create User
-        </Button>
-      </Tooltip> */}
-
+      <UserForm />
       <div>
         {users
           ? users.map((user) => (
@@ -66,26 +44,6 @@ export default function ManageUsers() {
             ))
           : null}
       </div>
-
-      {/* <Box
-        sx={{
-          bgcolor: "background.paper",
-          display: "block",
-          height: 224,
-          margin: 10,
-        }}
-      >
-        <Tooltip>
-          <Button
-            sx={{ marginBottom: "25px" }}
-            variant="outlined"
-            color="error"
-          >
-            {clientName}
-          </Button>
-        </Tooltip> */}
-      {/* <UserTransferList id={parseInt(id)} component={"users"} /> */}
-      {/* </Box> */}
     </>
   );
 }
