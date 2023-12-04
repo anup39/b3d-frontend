7fbf590806cdb2ebcc349ebc06373bfa4c5191d4
Anup Dahal <adahal39@gmail.com>
2023-12-04 21:55:28 +0545

    Added

--- src/components/Property/InputFileUpload.jsx
+++ src/components/Property/InputFileUpload.jsx
@@ -63,107 +63,116 @@ export default function InputFileUpload({
     setOpenrasterErrorToast(false);
     onDoneLoaded(false);
     onImage();
-    RemoveSourceAndLayerFromMap(
-      window.mapproperty,
-      "geojson-layer",
-      "geojson-source"
-    );
+    RemoveSourceAndLayerFromMap({
+      map: window.mapproperty,
+      layerId: "geojson-layer",
+      sourceId: "geojson-source",
+    });
     const file = e.target.files[0];
+    console.log(file, "file");
+    onFileUpload(file);
+    onDoneLoaded(true);
+    onFileName(file.name);
+    const file_size = bytesToMB(file.size);
+    onSetFilesize(file_size + " " + "MB");
+    onProjection(`EPSG:${4326}`);
+
     if (file && file.type === "image/tiff") {
       const reader = new FileReader();
-      reader.onload = async (e) => {
-        const arrayBuffer = e.target.result;
-        try {
-          const tiff = await fromArrayBuffer(arrayBuffer);
-          const image_raster = await tiff.getImage();
-          //   const data = await image.readRasters();
-          const bbox = image_raster.getBoundingBox();
-          const geo_Keys = image_raster.geoKeys;
-          // console.log(geo_Keys, "geokeys");
-          const source_bbox = {
-            southwest: [bbox[0], bbox[1]],
-            northeast: [bbox[2], bbox[3]],
-          };
-          const source_epsg = geo_Keys.ProjectedCSTypeGeoKey;
+      console.log(reader, "reader");
+      // reader.onload = async (e) => {
+      //   const arrayBuffer = e.target.result;
+      //   try {
+      //     const tiff = await fromArrayBuffer(arrayBuffer);
+      //     const image_raster = await tiff.getImage();
+      //     //   const data = await image.readRasters();
+      //     const bbox = image_raster.getBoundingBox();
+      //     const geo_Keys = image_raster.geoKeys;
+      //     console.log(geo_Keys, "geokeys");
+      //     const source_bbox = {
+      //       southwest: [bbox[0], bbox[1]],
+      //       northeast: [bbox[2], bbox[3]],
+      //     };
+      //     const source_epsg = geo_Keys.ProjectedCSTypeGeoKey;
 
-          if (epsgDefinitions.hasOwnProperty(source_epsg)) {
-            proj4.defs(`EPSG:${source_epsg}`, epsgDefinitions[source_epsg]);
-            proj4.defs("EPSG:4326", epsgDefinitions[4326]);
-            const bboxInEPSG4326 = {
-              southwest: proj4(
-                `EPSG:${source_epsg}`,
-                "EPSG:4326",
-                source_bbox.southwest
-              ),
-              northeast: proj4(
-                `EPSG:${source_epsg}`,
-                "EPSG:4326",
-                source_bbox.northeast
-              ),
-            };
+      //     if (epsgDefinitions.hasOwnProperty(source_epsg)) {
+      //       proj4.defs(`EPSG:${source_epsg}`, epsgDefinitions[source_epsg]);
+      //       proj4.defs("EPSG:4326", epsgDefinitions[4326]);
+      //       const bboxInEPSG4326 = {
+      //         southwest: proj4(
+      //           `EPSG:${source_epsg}`,
+      //           "EPSG:4326",
+      //           source_bbox.southwest
+      //         ),
+      //         northeast: proj4(
+      //           `EPSG:${source_epsg}`,
+      //           "EPSG:4326",
+      //           source_bbox.northeast
+      //         ),
+      //       };
 
-            const bbox_reprojected = [
-              bboxInEPSG4326.northeast[0],
-              bboxInEPSG4326.northeast[1],
-              bboxInEPSG4326.southwest[0],
-              bboxInEPSG4326.southwest[1],
-            ];
+      //       const bbox_reprojected = [
+      //         bboxInEPSG4326.northeast[0],
+      //         bboxInEPSG4326.northeast[1],
+      //         bboxInEPSG4326.southwest[0],
+      //         bboxInEPSG4326.southwest[1],
+      //       ];
 
-            const geoJSONPolygon = {
-              type: "Feature",
-              geometry: {
-                type: "Polygon",
-                coordinates: [
-                  [
-                    [bbox_reprojected[0], bbox_reprojected[1]],
-                    [bbox_reprojected[2], bbox_reprojected[1]],
-                    [bbox_reprojected[2], bbox_reprojected[3]],
-                    [bbox_reprojected[0], bbox_reprojected[3]],
-                    [bbox_reprojected[0], bbox_reprojected[1]],
-                  ],
-                ],
-              },
-              properties: {},
-            };
-            window.mapproperty.addSource("geojson-source", {
-              type: "geojson",
-              data: geoJSONPolygon,
-            });
+      //       const geoJSONPolygon = {
+      //         type: "Feature",
+      //         geometry: {
+      //           type: "Polygon",
+      //           coordinates: [
+      //             [
+      //               [bbox_reprojected[0], bbox_reprojected[1]],
+      //               [bbox_reprojected[2], bbox_reprojected[1]],
+      //               [bbox_reprojected[2], bbox_reprojected[3]],
+      //               [bbox_reprojected[0], bbox_reprojected[3]],
+      //               [bbox_reprojected[0], bbox_reprojected[1]],
+      //             ],
+      //           ],
+      //         },
+      //         properties: {},
+      //       };
+      //       window.mapproperty.addSource("geojson-source", {
+      //         type: "geojson",
+      //         data: geoJSONPolygon,
+      //       });
 
-            window.mapproperty.addLayer({
-              id: "geojson-layer",
-              type: "line",
-              source: "geojson-source",
-              layout: {},
-              paint: {
-                "line-color": "red",
-                "line-opacity": 1,
-              },
-            });
+      //       window.mapproperty.addLayer({
+      //         id: "geojson-layer",
+      //         type: "line",
+      //         source: "geojson-source",
+      //         layout: {},
+      //         paint: {
+      //           "line-color": "red",
+      //           "line-opacity": 1,
+      //         },
+      //       });
 
-            window.mapproperty.fitBounds(bbox_reprojected);
-            window.mapproperty.on("moveend", function () {
-              onDoneLoaded(true);
-            });
-          } else {
-            setOpenrasterErrorToast(true);
-            setOpenrasterErrorMessage(`${source_epsg} cannot be uploaded.`);
-          }
+      //       window.mapproperty.fitBounds(bbox_reprojected);
+      //       window.mapproperty.on("moveend", function () {
+      //         onDoneLoaded(true);
+      //       });
+      //     } else {
+      //       setOpenrasterErrorToast(true);
+      //       setOpenrasterErrorMessage(`${source_epsg} cannot be uploaded.`);
+      //     }
 
-          onFileName(file.name);
-          const file_size = bytesToMB(file.size);
-          onSetFilesize(file_size + " " + "MB");
-          onProjection(`EPSG:${source_epsg}`);
+      //     onFileName(file.name);
+      //     const file_size = bytesToMB(file.size);
+      //     onSetFilesize(file_size + " " + "MB");
+      //     onProjection(`EPSG:${source_epsg}`);
 
-          if (onFileUpload) {
-            onFileUpload(file);
-          }
-        } catch (error) {
-          setOpenrasterErrorToast(true);
-          setOpenrasterErrorMessage("Please select a valid GeoTIFF file.");
-        }
-      };
-      reader.readAsArrayBuffer(file);
+      //     if (onFileUpload) {
+      //       onFileUpload(file);
+      //     }
+      //   } catch (error) {
+      //     setOpenrasterErrorToast(true);
+      //     setOpenrasterErrorMessage("Please select a valid GeoTIFF file.");
+      //   }
+      // };
+      // reader.readAsArrayBuffer(file);
     } else {
       setOpenrasterErrorToast(true);
       setOpenrasterErrorMessage("Please select a tiff file.");
