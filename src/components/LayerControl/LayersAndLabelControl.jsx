b993e5898d64ffb652069244d40cd08b78514af5
Anup Dahal <adahal39@gmail.com>
2023-12-22 23:22:55 +0545

    Css extrae element

--- src/components/LayerControl/LayersAndLabelControl.jsx
+++ src/components/LayerControl/LayersAndLabelControl.jsx
@@ -1,6 +1,8 @@
 import LayersControlPanel from "./LayerControlPanel";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Tooltip, Typography } from "@mui/material";
 import { PropTypes } from "prop-types";
+import SummarizeIcon from "@mui/icons-material/Summarize";
+import BackupIcon from "@mui/icons-material/Backup";
 
 export default function LayersAndLabelControl({ map }) {
   return (
@@ -9,12 +11,19 @@ export default function LayersAndLabelControl({ map }) {
       style={{ display: "flex", flexDirection: "column" }}
     >
       <Box>
-        <Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
           <Typography
             sx={{
               backgroundColor: "white",
               fontWeight: "bold",
               marginTop: 1,
+              marginBottom: 0,
               paddingTop: 0,
               marginLeft: 2,
               fontSize: "14px",
@@ -23,6 +32,23 @@ export default function LayersAndLabelControl({ map }) {
           >
             Measurings : <span style={{ color: "#D71A60" }}>Map nov</span>
           </Typography>
+          <Box></Box>
+
+          <Tooltip title="Report">
+            <SummarizeIcon
+              sx={{ "&:hover": { cursor: "pointer" }, mt: 1, color: "#d61b60" }}
+            />
+          </Tooltip>
+          <Tooltip title="Export">
+            <BackupIcon
+              sx={{
+                "&:hover": { cursor: "pointer" },
+                mt: 1,
+                mr: 1,
+                color: "#d61b60",
+              }}
+            />
+          </Tooltip>
         </Box>
 
         <LayersControlPanel map={map} />
