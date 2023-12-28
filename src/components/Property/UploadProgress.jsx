5519929c554bfa27e062a6de82064d65bf545604
Anup Dahal <adahal39@gmail.com>
2023-12-28 09:02:24 +0545

    New logic and component refactored

--- src/components/Property/UploadProgress.jsx
+++ src/components/Property/UploadProgress.jsx
@@ -2,64 +2,59 @@ import Grid from "@mui/material/Grid";
 import LinearProgressLabel from "../Property/LinearProgressLabel";
 import { Button } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import PropTypes from "prop-types";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setshowProgressFormOpen } from "../../reducers/Property";
 
 export default function UploadProgress() {
-  const showProgressFormOpen = useSelector(
-    (state) => state.property.showProgressFormOpen
-  );
+  const dispatch = useDispatch();
   const progress = useSelector((state) => state.property.progress);
   return (
     <>
-      {showProgressFormOpen && (
-        <div
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          background: "rgba(0, 0, 0, 0.5)",
+          zIndex: 9999,
+        }}
+      >
+        <form
           style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            background: "rgba(0, 0, 0, 0.5)",
-            zIndex: 9999,
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "300px",
+            background: "#fff",
+            padding: "20px",
+            zIndex: 10000,
           }}
         >
-          <form
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: "300px",
-              background: "#fff",
-              padding: "20px",
-              zIndex: 10000,
-            }}
-          >
-            <Grid item xs={12}>
-              <Typography variant="body2" gutterBottom>
-                Uploading ... Please Wait:
-              </Typography>
-            </Grid>
-            <LinearProgressLabel value={progress}></LinearProgressLabel>
+          <Grid item xs={12}>
+            <Typography variant="body2" gutterBottom>
+              Uploading ... Please Wait:
+            </Typography>
+          </Grid>
+          <LinearProgressLabel value={progress}></LinearProgressLabel>
 
-            <Grid item xs={12}>
-              <Button
-                onClick={() => {
-                  setshowProgressFormOpen(false);
-                }}
-                variant="contained"
-                color="error"
-                size="small"
-                fullWidth
-              >
-                Cancel
-              </Button>
-            </Grid>
-          </form>
-        </div>
-      )}
+          <Grid item xs={12}>
+            <Button
+              onClick={() => {
+                dispatch(setshowProgressFormOpen(false));
+              }}
+              variant="contained"
+              color="error"
+              size="small"
+              fullWidth
+            >
+              Cancel
+            </Button>
+          </Grid>
+        </form>
+      </div>
     </>
   );
 }
--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom";
 import AppBar from "../components/Common/AppBar";
-import PropertyForm from "../components/Property/PropertyForm";
 import UploadProgress from "../components/Property/UploadProgress";
 import PropertyContainer from "../components/Property/PropertyContainer";
+import AddPropertyButton from "../components/Property/AddPropertyButton";
+import UploadPropertyForm from "../components/Property/UploadPropertyForm";
+import { useSelector } from "react-redux";
 
 export default function Properties() {
   const { client_id } = useParams();
   const { project_id } = useParams();
+  const showTifUpload = useSelector(
+    (state) => state.displaySettings.showTifUpload
+  );
+  const showProgressFormOpen = useSelector(
+    (state) => state.property.showProgressFormOpen
+  );
+
   // const [isProgressFormOpen, setIsProgressFormOpen] = useState(false);
   // const [progress, setProgress] = useState(0);
 
@@ -21,8 +30,11 @@ export default function Properties() {
   return (
     <>
       <AppBar></AppBar>
-      <PropertyForm client_id={client_id} project_id={project_id} />
-      <UploadProgress />
+      <AddPropertyButton />
+      {showTifUpload ? (
+        <UploadPropertyForm project_id={project_id} client_id={client_id} />
+      ) : null}
+      {showProgressFormOpen ? <UploadProgress /> : null}
       <PropertyContainer project_id={project_id} />
     </>
   );
