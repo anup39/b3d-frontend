f0132679e21c0ef860de8d95551f75ba4afc5b0d
Anup Dahal <adahal39@gmail.com>
2024-01-13 20:48:31 +0545

    Added a measuring tabzle logic

--- src/components/TableMeasuringMapControl/TableMesuringsForMap.jsx
+++ src/components/TableMeasuringMapControl/TableMesuringsForMap.jsx
@@ -7,8 +7,8 @@ import {
   setTableSummationData,
   setTableSummationDataColumns,
 } from "../../reducers/MapView";
-import PolylineIcon from "@mui/icons-material/Polyline";
-import MyLocationIcon from "@mui/icons-material/MyLocation";
+import RectangleIcon from "@mui/icons-material/Rectangle";
+import CircleIcon from "@mui/icons-material/Circle";
 import ShowChartIcon from "@mui/icons-material/ShowChart";
 import { render } from "react-dom";
 
@@ -18,9 +18,9 @@ const renderCell = (params) => {
   let icon;
 
   if (type_of_geometry === "Polygon") {
-    icon = <PolylineIcon sx={{ color: color }} />;
+    icon = <RectangleIcon sx={{ color: color }} />;
   } else if (type_of_geometry === "Point") {
-    icon = <MyLocationIcon sx={{ color: color }} />;
+    icon = <CircleIcon sx={{ color: color }} />;
   } else {
     icon = <ShowChartIcon sx={{ color: color }} />;
   }
@@ -29,67 +29,44 @@ const renderCell = (params) => {
 };
 
 const columns = [
-  {
-    field: "id",
-    headerName: "id",
-    width: 80,
-    type: "string",
-    editable: false,
-  },
+  // {
+  //   field: "id",
+  //   headerName: "id",
+  //   width: 80,
+  //   type: "number",
+  //   editable: false,
+  // },
   {
     field: "symbol",
     headerName: "symbol",
-    width: 150,
-    type: "object",
+    type: "string",
+    width: 80,
     renderCell: renderCell,
   },
-
   {
-    field: "standard_category",
-    headerName: "standard_category",
-    width: 150,
-    type: "string",
-    editable: false,
-  },
-  {
-    field: "sub_category",
-    type: "string",
-    width: 150,
-    editable: false,
-    headerName: "sub_category",
-  },
-  {
-    field: "category",
-    type: "string",
-    width: 150,
-    editable: false,
+    field: "view_name",
     headerName: "category",
-  },
-  {
-    field: "type_of_geometry",
     type: "string",
-    width: 150,
-    editable: false,
-    headerName: "type_of_geometry",
+    width: 290,
   },
   {
     field: "description",
     type: "string",
-    width: 150,
+    width: 140,
     editable: false,
     headerName: "description",
   },
   {
     field: "area",
-    type: "string",
-    width: 150,
+    type: "number",
+    width: 50,
     editable: false,
     headerName: "area",
   },
   {
     field: "length",
-    type: "string",
-    width: 150,
+    type: "number",
+    width: 90,
     editable: false,
     headerName: "length",
   },
@@ -98,10 +75,7 @@ const columns = [
 const rows = [
   {
     id: 1,
-    standard_category: "Grass",
-    sub_category: "Grass Green",
-    category: "Grass Green Tall",
-    type_of_geometry: "Polygon",
+    view_name: "Grass Green Short",
     description: "Grass measurings",
     area: 45,
     length: 56,
@@ -109,10 +83,7 @@ const rows = [
   },
   {
     id: 2,
-    standard_category: "Grass",
-    sub_category: "Grass Green",
-    category: "Grass Green Short",
-    type_of_geometry: "Polygon",
+    view_name: "Grass Green Short",
     description: "Grass measurings",
     area: 45,
     length: 56,
@@ -120,55 +91,40 @@ const rows = [
   },
   {
     id: 3,
-    standard_category: "Lake",
-    sub_category: "Lake Depth",
-    category: "Lake Depth Old",
-    type_of_geometry: "LineString",
-    description: "Lake measurings",
+    view_name: "Grass Green Short",
+    description: "Grass measurings",
     area: 40,
     length: 560,
     symbol: { color: "green", type_of_geometry: "LineString" },
   },
   {
     id: 4,
-    standard_category: "Pole",
-    sub_category: "Pole Electrical",
-    category: "Pole Electrical Rural",
-    type_of_geometry: "Point",
-    description: "Pole measurings",
+    view_name: "Grass Green Short",
+    description: "Grass measurings",
     area: 450,
     length: 560,
     symbol: { color: "blue", type_of_geometry: "Point" },
   },
   {
     id: 5,
-    standard_category: "Pole",
-    sub_category: "Pole Electrical",
-    category: "Pole Electrical Residental",
-    type_of_geometry: "Point",
-    description: "Pole measurings",
+    view_name: "Grass Green Short",
+    description: "Grass measurings",
     area: 450,
     length: 560,
     symbol: { color: "blue", type_of_geometry: "Point" },
   },
   {
     id: 6,
-    standard_category: "Pole",
-    sub_category: "Pole Electrical",
-    category: "Pole Electrical Residental",
-    type_of_geometry: "Point",
-    description: "Pole measurings",
+    view_name: "Grass Green Short",
+    description: "Grass measurings",
     area: 450,
     length: 560,
     symbol: { color: "yellow", type_of_geometry: "Point" },
   },
   {
     id: 7,
-    standard_category: "Pole",
-    sub_category: "Pole Electrical",
-    category: "Pole Electrical Residental",
-    type_of_geometry: "Point",
-    description: "Pole measurings",
+    view_name: "Grass Green Short",
+    description: "Grass measurings",
     area: 450,
     length: 560,
     symbol: "red",
@@ -200,14 +156,13 @@ export default function TableMeasuringsForMap() {
         }/measuring-table-summation/?client=${currentClient}&project=${currentProject}`
       )
       .then((res) => {
-        if (showTableMeasurings && res.data.columns.length > 0) {
-          // dispatch(setTableSummationData(res.data));
-          dispatch(setTableSummationDataColumns(res.data.columns));
-
-          console.log(res.data.columns);
-        } else {
-          dispatch(setTableSummationDataColumns([]));
-        }
+        // if (showTableMeasurings && res.data.columns.length > 0) {
+        //   // dispatch(setTableSummationData(res.data));
+        //   // dispatch(setTableSummationDataColumns(res.data.columns));
+        //   console.log(res.data.columns);
+        // } else {
+        //   dispatch(setTableSummationDataColumns([]));
+        // }
       });
   }, [showTableMeasurings, currentClient, currentProject, dispatch]);
   return (
