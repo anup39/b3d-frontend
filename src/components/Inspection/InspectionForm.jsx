8e3156a53ae813c5340851a029941d4455e223b5
Anup Dahal <adahal39@gmail.com>
2024-01-18 10:30:07 +0545

    Fixed issues and refactored code

--- src/components/Inspection/InspectionForm.jsx
+++ src/components/Inspection/InspectionForm.jsx
@@ -1,25 +1,57 @@
 import { Button, Tooltip } from "@mui/material";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { CircularProgress } from "@mui/material";
 import PropTypes from "prop-types";
+import {
+  setshowToast,
+  settoastMessage,
+  settoastType,
+} from "../../reducers/DisplaySettings";
 
 export default function InspectionForm({ client_id, project_id }) {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const [selectedDate, setSelectedDate] = useState(null);
   const [isFormOpen, setIsFormOpen] = useState(false);
-
-  console.log(client_id, project_id);
+  const [loading, setLoading] = useState(false);
+  const user_id = useSelector((state) => state.auth.user_id);
 
   const openForm = () => {
     setIsFormOpen(true);
   };
   const closeForm = () => {
     setIsFormOpen(false);
+  };
+
+  const handleCreateInspection = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const data = new FormData(event.currentTarget);
+    console.log(selectedDate, "selectedDate");
+    data.append("date", selectedDate);
+    data.append("client", client_id);
+    data.append("project", project_id);
+    data.append("created_by", user_id);
+    data.append("is_display", true);
+    console.log(data, "final paylaod");
+
+    // #Todo
+
+    setTimeout(() => {
+      setLoading(false);
+      // API call post request to send the data remaining
+      // Loader and Toast Remaining
+      dispatch(setshowToast(true));
+      dispatch(settoastMessage("Successfully Created Inspection"));
+      dispatch(settoastType("success"));
+      closeForm();
+    }, 5000);
   };
   return (
     <>
@@ -42,11 +74,11 @@ export default function InspectionForm({ client_id, project_id }) {
             width: "100%",
             height: "100%",
             background: "rgba(0, 0, 0, 0.5)",
-            zIndex: 9999,
+            // zIndex: 9999,
           }}
         >
           <form
-            // onSubmit={handleCreateProject}
+            onSubmit={handleCreateInspection}
             style={{
               position: "absolute",
               top: "50%",
@@ -73,35 +105,23 @@ export default function InspectionForm({ client_id, project_id }) {
               </Grid>
               <Grid item xs={12}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    label="Date"
-                    value={selectedDate}
-                    onChange={(newValue) => {
-                      setSelectedDate(newValue);
-                    }}
-                    renderInput={() => (
-                      <TextField
-                        id="Date"
-                        name="Date"
-                        label="Date"
-                        variant="outlined"
-                        size="small"
-                        InputLabelProps={{ shrink: true }}
-                        required
-                        fullWidth
-                      />
-                    )}
-                  />
+                  <DemoContainer components={["DatePicker"]}>
+                    <DatePicker
+                      onChange={(newValue) => setSelectedDate(newValue)}
+                      label="Pick a date "
+                    />
+                  </DemoContainer>
                 </LocalizationProvider>
               </Grid>
               <Grid item xs={12}>
                 <Button
                   type="submit"
                   fullWidth
-                  variant="contained"
+                  variant={loading ? "outlined" : "contained"}
                   sx={{ mt: 3, mb: 2 }}
                 >
-                  Add Inspection
+                  {loading ? null : "Add Inspection"}
+                  {loading ? <CircularProgress /> : null}
                 </Button>
               </Grid>
               <Grid item xs={12}>
