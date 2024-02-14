5a2ca929b6941e836a957c25a0131b5884026ec2
Anup Dahal <adahal39@gmail.com>
2024-02-14 21:36:23 +0545

    Property list view

--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -13,6 +13,7 @@ import {
 import MapView from "../components/MapView/MapView";
 import axios from "axios";
 import { setprojects } from "../reducers/Project";
+import { setClientDetail } from "../reducers/MapView";
 
 export default function Properties() {
   const { client_id, project_id, view } = useParams();
@@ -41,6 +42,17 @@ export default function Properties() {
       )
       .then((res) => {
         dispatch(setprojects(res.data));
+      });
+
+    axios
+      .get(`${import.meta.env.VITE_API_DASHBOARD_URL}/clients/${client_id}/`)
+      .then((res) => {
+        const client_detail = {
+          client_id: client_id,
+          client_name: res.data.name,
+          client_image: res.data.name.charAt(0).toUpperCase(),
+        };
+        dispatch(setClientDetail(client_detail));
       });
   }, [client_id, project_id, dispatch]);
 
