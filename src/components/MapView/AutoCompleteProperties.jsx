e26f49f3fabb3353a06fa566d4e07b53f35fb7a6
Anup Dahal <adahal39@gmail.com>
2024-02-16 22:06:49 +0545

    Search functionality added

--- src/components/MapView/AutoCompleteProperties.jsx
+++ src/components/MapView/AutoCompleteProperties.jsx
@@ -50,59 +50,23 @@ export default function AutoCompleteProperties({ open, client_id }) {
       });
   }, [dispatch, client_id]);
   return (
-    <Autocomplete
-      open={false}
-      disablePortal
-      size="small"
-      id="category-select"
-      options={options}
-      getOptionLabel={(option) => option.name}
-      sx={{ width: 200, my: 1, ml: 2, display: open ? "block" : "none" }}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label={"Search Properties"}
-          variant="outlined"
-          value={inputValue}
-          onChange={(event) => {
-            console.log(event.target.value, "event.target.value");
-            // setInputValue(event.target.value);
-            // if (event.target.value === "") {
-            //   axios
-            //     .get(
-            //       `${
-            //         import.meta.env.VITE_API_DASHBOARD_URL
-            //       }/projects/?client=${client_id}`,
-            //       {
-            //         headers: {
-            //           Authorization: "Token " + localStorage.getItem("token"),
-            //         },
-            //       }
-            //     )
-            //     .then((res) => {
-            //       setOptions(res.data);
-            //     })
-            //     .catch((error) => {
-            //       console.error("Error fetching data:", error);
-            //     });
-            // }
-          }}
-        />
-      )}
-      onInputChange={(event, newInputValue) => {
-        setInputValue(newInputValue);
-        fetchProjects(newInputValue);
+    <TextField
+      sx={{
+        width: 200,
+        fontFamily: "Roboto",
+        fontSize: "7px",
+        marginTop: "10px",
+        marginBottom: "10px",
+        marginLeft: "15px",
       }}
-      // onChange={(event, newValue) => {
-      //   if (newValue) {
-      //     setInputValue(newValue.name);
-      //     // onItemSelected(newValue.id, newValue.standard_category);
-      //     console.log(newValue, "newvalue");
-
-      //     console.log(newValue, "newvalue");
-      //     fetchProjects(newValue.name);
-      //   }
-      // }}
+      size="small"
+      onChange={(event) => {
+        setInputValue(event.target.value);
+        fetchProjects(event.target.value);
+      }}
+      id="search-properties"
+      label="Search Properties"
+      variant="outlined"
     />
   );
 }
