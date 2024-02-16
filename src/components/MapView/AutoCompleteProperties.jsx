e4130a4ac1c035fcf8ca04791ef86998abf7af77
Anup Dahal <adahal39@gmail.com>
2024-02-16 22:01:45 +0545

    Search api functionality added

--- src/components/MapView/AutoCompleteProperties.jsx
+++ src/components/MapView/AutoCompleteProperties.jsx
@@ -5,11 +5,29 @@ import axios from "axios";
 import PropTypes from "prop-types";
 import { setprojects } from "../../reducers/Project";
 import { useDispatch } from "react-redux";
+import { debounce } from "lodash";
 
 export default function AutoCompleteProperties({ open, client_id }) {
   const dispatch = useDispatch();
   const [options, setOptions] = useState([]);
   const [inputValue, setInputValue] = useState("");
+
+  const fetchProjects = debounce((searchTerm) => {
+    axios
+      .get(
+        `${
+          import.meta.env.VITE_API_DASHBOARD_URL
+        }/projects/?client=${client_id}&search=${searchTerm}`,
+        {
+          headers: {
+            Authorization: "Token " + localStorage.getItem("token"),
+          },
+        }
+      )
+      .then((res) => {
+        dispatch(setprojects(res.data));
+      });
+  }, 100); // delay in milliseconds
 
   useEffect(() => {
     axios
@@ -33,6 +51,7 @@ export default function AutoCompleteProperties({ open, client_id }) {
   }, [dispatch, client_id]);
   return (
     <Autocomplete
+      open={false}
       disablePortal
       size="small"
       id="category-select"
@@ -47,51 +66,43 @@ export default function AutoCompleteProperties({ open, client_id }) {
           value={inputValue}
           onChange={(event) => {
             console.log(event.target.value, "event.target.value");
-            setInputValue(event.target.value);
-            if (event.target.value === "") {
-              axios
-                .get(
-                  `${
-                    import.meta.env.VITE_API_DASHBOARD_URL
-                  }/projects/?client=${client_id}`,
-                  {
-                    headers: {
-                      Authorization: "Token " + localStorage.getItem("token"),
-                    },
-                  }
-                )
-                .then((res) => {
-                  setOptions(res.data);
-                })
-                .catch((error) => {
-                  console.error("Error fetching data:", error);
-                });
-            }
+            // setInputValue(event.target.value);
+            // if (event.target.value === "") {
+            //   axios
+            //     .get(
+            //       `${
+            //         import.meta.env.VITE_API_DASHBOARD_URL
+            //       }/projects/?client=${client_id}`,
+            //       {
+            //         headers: {
+            //           Authorization: "Token " + localStorage.getItem("token"),
+            //         },
+            //       }
+            //     )
+            //     .then((res) => {
+            //       setOptions(res.data);
+            //     })
+            //     .catch((error) => {
+            //       console.error("Error fetching data:", error);
+            //     });
+            // }
           }}
         />
       )}
-      onChange={(event, newValue) => {
-        if (newValue) {
-          setInputValue(newValue.name);
-          // onItemSelected(newValue.id, newValue.standard_category);
-          console.log(newValue, "newvalue");
-
-          axios
-            .get(
-              `${
-                import.meta.env.VITE_API_DASHBOARD_URL
-              }/projects/?client=${client_id}&id=${newValue.id}`,
-              {
-                headers: {
-                  Authorization: "Token " + localStorage.getItem("token"),
-                },
-              }
-            )
-            .then((res) => {
-              dispatch(setprojects(res.data));
-            });
-        }
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+        fetchProjects(newInputValue);
       }}
+      // onChange={(event, newValue) => {
+      //   if (newValue) {
+      //     setInputValue(newValue.name);
+      //     // onItemSelected(newValue.id, newValue.standard_category);
+      //     console.log(newValue, "newvalue");
+
+      //     console.log(newValue, "newvalue");
+      //     fetchProjects(newValue.name);
+      //   }
+      // }}
     />
   );
 }
