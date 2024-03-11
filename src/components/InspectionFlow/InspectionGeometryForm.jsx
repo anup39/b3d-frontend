487a3a0169c6ef132c527cf6616a04088cb7f0ce
Anup Dahal <adahal39@gmail.com>
2024-03-11 19:32:20 +0545

    Things to revise

--- src/components/InspectionFlow/InspectionGeometryForm.jsx
+++ src/components/InspectionFlow/InspectionGeometryForm.jsx
@@ -220,3 +220,7 @@ InspectionGeometryForm.propTypes = {
   loaderSave: PropTypes.bool,
   loaderDelete: PropTypes.bool,
 };
+
+// # things they are money already spent on
+// # things they are money to be spent on
+// # things they are money to be spent on
