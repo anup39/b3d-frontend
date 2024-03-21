05e8b5c0fbebb928ef20bb0553ccf0afe2546865
Anup Dahal <adahal39@gmail.com>
2024-03-21 23:41:59 +0545

    Fixed the new changes

--- src/components/DrawControl/Cancel.jsx
+++ src/components/DrawControl/Cancel.jsx
@@ -60,6 +60,7 @@ export default function Cancel() {
   // here fix the current time and index
   // and dispatch the action to update the current time and index
   // and then call the function to update the time and index
+  // Again new changes for the package
 
   return (
     <div>
