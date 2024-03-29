123aaa5dfbb6072db6dea610ada22f3079c1543a
Anup Dahal <adahal39@gmail.com>
2024-03-29 19:59:12 +0545

    clear search field

--- src/components/MapView/AutoCompleteProperties.jsx
+++ src/components/MapView/AutoCompleteProperties.jsx
@@ -1,32 +1,57 @@
 import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import CloseIcon from "@mui/icons-material/Close";
+
 import { useDispatch, useSelector } from "react-redux";
 import { filterProjects } from "../../reducers/Project";
 import { setSearchText } from "../../reducers/MapView";
+import { IconButton, Tooltip } from "@mui/material";
 
 export default function AutoCompleteProperties() {
   const dispatch = useDispatch();
   const searchText = useSelector((state) => state.mapView.searchText);
 
   return (
-    <TextField
+    <Box
       sx={{
-        width: 200,
-        fontFamily: "Roboto",
-        fontSize: "7px",
-        marginTop: "10px",
-        marginBottom: "10px",
-        marginLeft: "15px",
+        display: "flex",
+        alingItems: "center",
+        justifyContent: "flex-start",
       }}
-      size="small"
-      value={searchText}
-      onChange={(event) => {
-        dispatch(filterProjects(event.target.value));
-        dispatch(setSearchText(event.target.value));
-      }}
-      id="search-properties"
-      label="Search Properties"
-      variant="outlined"
-    />
+    >
+      <TextField
+        sx={{
+          width: 175,
+          fontFamily: "Roboto",
+          fontSize: "5px",
+          marginTop: "10px",
+          marginBottom: "10px",
+          marginLeft: "15px",
+        }}
+        size="small"
+        value={searchText}
+        onChange={(event) => {
+          dispatch(filterProjects(event.target.value));
+          dispatch(setSearchText(event.target.value));
+        }}
+        id="search-properties"
+        label="Search Properties"
+        variant="outlined"
+      />
+
+      <IconButton
+        sx={{ borderRadius: 0 }}
+        onClick={() => {
+          dispatch(filterProjects(""));
+          dispatch(setSearchText(""));
+        }}
+        size="small"
+      >
+        <Tooltip title="Clear Search" placement="top">
+          <CloseIcon sx={{ fontSize: 20, color: "#E91E62" }} />
+        </Tooltip>
+      </IconButton>
+    </Box>
   );
 }
 
