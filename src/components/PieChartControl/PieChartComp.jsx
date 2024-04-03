9209ec06f89769cbe923e54ac05a782de3b3d006
Anup Dahal <adahal39@gmail.com>
2024-04-03 16:12:13 +0545

    Added a mui pie charts

--- src/components/PieChartControl/PieChartComp.jsx
+++ src/components/PieChartControl/PieChartComp.jsx
@@ -1,7 +1,34 @@
-import PieCharts from "./PieCharts";
+// import PieCharts from "./PieCharts";
+import PieChartNew from "./PieChartNew";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import { useSelector } from "react-redux";
+import Tooltip from "@mui/material/Tooltip";
+import { useDispatch } from "react-redux";
+import { setshowPiechart } from "../../reducers/MapView";
 
 export default function PieChartComp() {
+  const dispatch = useDispatch();
   const showPiechart = useSelector((state) => state.mapView.showPiechart);
-  return <>{showPiechart ? <PieCharts></PieCharts> : null}</>;
+
+  return (
+    <>
+      {showPiechart ? (
+        <>
+          <Tooltip placement="top-end" title="Close Pie Chart">
+            <HighlightOffIcon
+              onClick={() => {
+                dispatch(setshowPiechart(false));
+              }}
+              sx={{
+                float: "right",
+                color: "#E91E62",
+                "&:hover": { cursor: "pointer" },
+              }}
+            />
+            <PieChartNew />
+          </Tooltip>
+        </>
+      ) : null}
+    </>
+  );
 }
---
+++ src/components/PieChartControl/PieChartNew.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { PieChart } from "@mui/x-charts/PieChart";
+
+const data = [
+  { id: 0, value: 10, label: "series A" },
+  { id: 1, value: 15, label: "series B" },
+  { id: 2, value: 20, label: "series C" },
+];
+
+export default function PieChartNew() {
+  return (
+    <PieChart
+      series={[
+        {
+          data,
+          highlightScope: { faded: "global", highlighted: "item" },
+          faded: { innerRadius: 10, additionalRadius: -10, color: "gray" },
+        },
+      ]}
+      width={350}
+      height={200}
+    />
+  );
+}
