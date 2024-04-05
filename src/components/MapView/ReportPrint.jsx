9b878522c8afe76d7206cf1aed5bde29d8d305aa
Anup Dahal <adahal39@gmail.com>
2024-04-05 18:53:06 +0545

    Reafactored code base

--- src/components/MapView/ReportPrint.jsx
+++ src/components/MapView/ReportPrint.jsx
@@ -13,10 +13,6 @@ import {
   setshowTableMeasurings,
 } from "../../reducers/MapView";
 import PieChartComp from "../PieChartControl/PieChartComp";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
 
 export default function ReportPrint() {
   const dispatch = useDispatch();
