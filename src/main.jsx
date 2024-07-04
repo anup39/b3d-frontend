df6182562e2f7cf5813bd5ae3f978d9a3c28392c
Subodhghimire99 <subodhghimire99@gmail.com>
2024-07-04 18:06:27 +0545

    Feat: Responsive navbar added to layoutNew

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,24 @@ import { store } from "./store";
 import { Provider } from "react-redux";
 import "./i18n.js";
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, createTheme } from "@mantine/core";
+
+const theme = createTheme({
+  breakpoints: {
+    xxs: "10em",
+    exs: "20em",
+    xs: "30em",
+    sm: "48em",
+    md: "64em",
+    lg: "74em",
+    xl: "90em",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <MantineProvider>
+      <MantineProvider theme={theme}>
         <App />
       </MantineProvider>
     </Provider>
