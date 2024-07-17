a54e96dd2a64ee8d4ec67d10d608b115ef23eaa9
Subodhghimire99 <subodhghimire99@gmail.com>
2024-07-17 11:04:56 +0545

    Fix: When editing the client form double api is called

--- src/components/Client/NewClientForm.jsx
+++ src/components/Client/NewClientForm.jsx
@@ -15,6 +15,7 @@ import { fetchClientDetailsByClientId } from "../../api/api";
 import PropTypes from "prop-types";
 import {
   useCreateClientMutation,
+  useGetClientsByClientIdQuery,
   useUpdateClientByIdMutation,
 } from "../../api/clientApi";
 
@@ -38,6 +39,12 @@ export default function NewClientForm({ id, closeEditForm }) {
 
   const [updateClientById] = useUpdateClientByIdMutation();
   const [createClient] = useCreateClientMutation();
+  const { refetch: refetchClientsById } = useGetClientsByClientIdQuery(
+    {
+      client_id: id,
+    },
+    { skip: !id }
+  );
 
   const closeForm = useCallback(() => {
     if (closeEditForm) {
@@ -49,10 +56,11 @@ export default function NewClientForm({ id, closeEditForm }) {
   useEffect(() => {
     if (id) {
       setLoading(true);
-      fetchClientDetailsByClientId(id)
+      refetchClientsById()
+        .unwrap()
         .then((response) => {
           setLoading(false);
-          setClientData(response);
+          setClientData(response[0]);
         })
         .catch((error) => {
           const error_message = error?.message;
