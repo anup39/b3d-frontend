1b8ee1e8ebdc240fcbee83e6eb7319d71ec0a81b
Subodhghimire99 <subodhghimire99@gmail.com>
2024-07-17 10:35:09 +0545

    Fix: total properties not uploaded in client card

---
+++ src/hooks/useEffectOnlyAfterMount.js
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from "react";
+
+
+//useEffect which runs only after mounting
+function useEffectOnlyAfterMount(fn, inputs) {
+  const isMountingRef = useRef(false);
+
+  useEffect(() => {
+    isMountingRef.current = true;
+  }, []);
+
+  useEffect(() => {
+    if (!isMountingRef.current) {
+      return fn();
+    } else {
+      isMountingRef.current = false;
+    }
+  }, inputs);
+}
+
+export default useEffectOnlyAfterMount;
